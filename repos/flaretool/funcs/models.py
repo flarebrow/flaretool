d18acd6b12f07622015d47dca3a7db72c701605c
flarebrow <github@flarebrow.com>
2023-09-02 17:20:19 +0900

    new pydantic support

--- repos/flaretool/funcs/models.py
+++ repos/flaretool/funcs/models.py
@@ -1,15 +1,16 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
+from typing import Union
 from flaretool.basemodels import BaseDataModel
 
 
 class AmazonInfo(BaseDataModel):
     result: bool
-    url: str = None
-    title: str = None
-    price: int = None
-    stock: str = None
-    distributor: str = None
-    sender: str = None
-    evaluation: str = None
+    url: Union[str, None] = None
+    title: Union[str, None] = None
+    price: Union[str, int, None] = None
+    stock: Union[str, None] = None
+    distributor: Union[str, None] = None
+    sender: Union[str, None] = None
+    evaluation: Union[str, None] = None
